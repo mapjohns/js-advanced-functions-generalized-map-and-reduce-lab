435f85d5d754d7f6bda91312385fb42be785473e
Matthew <mattj20@gmail.com>
2022-10-25 17:45:16 -0700

    commit tree 9eccbd9eba312d96758b4c686d7bf422345a43b4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Add your functions here
+
+function map(array, func) {
+    return array.map(a => func(a))
+}
+
+function reduce(array, func, startVal=array[0]) {
+    return array.reduce((startVal, curr) => func(startVal, curr), startVal)
+}
